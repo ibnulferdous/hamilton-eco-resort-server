358a23a328225d523198c336fb2ab10c37ca544b
Ibnul Ferdous <ibnul04@gmail.com>
2021-10-31 22:20:22 +0600

    APIs added

--- index.js
+++ index.js
@@ -18,15 +18,26 @@ async function run() {
     await client.connect();
     const database = client.db("hamilton_eco_resort");
     const villasCollection = database.collection("villas");
+    const bookingCollection = database.collection("bookings");
 
-    // GET API
+    
+
+    // Villas- GET API for all
     app.get('/villas', async(req, res) => {
       const cursor = villasCollection.find({});
       const villas = await cursor.toArray();
       res.send(villas);
     })
 
-    // POST API
+    // Villas- GET API for one villa
+    app.get('/villas/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const villa = await villasCollection.findOne(query);
+      res.json(villa);
+    })
+
+    // Villas- POST API
     app.post('/villas', async(req, res) => {
       const villa = req.body;
       const result = await villasCollection.insertOne(villa);
@@ -34,6 +45,59 @@ async function run() {
       console.log(`A document was inserted with the _id: ${result.insertedId}`);
       res.json(result);
     });
+
+    // Bookings- GET API for all
+    app.get('/bookings', async(req, res) => {
+      const cursor = bookingCollection.find({});
+      const allBookings = await cursor.toArray();
+      res.send(allBookings);
+    })
+
+    // Bookings- POST API
+    app.post('/bookings', async(req, res) => {
+      const booking = req.body;
+
+      const result = await bookingCollection.insertOne(booking);
+
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.json(result);
+    });
+
+    // Bookings- POST API to find data by googleId
+    app.post('/bookings/bygoogleid', async(req, res) => {
+      const userId = req.body;
+      const query = { googleId: {$in: userId} };
+      const bookingByUser = await bookingCollection.find(query).toArray();
+      res.json(bookingByUser);
+    })
+
+    // Bookings- UPDATE API
+    app.put('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateStatus = {
+        $set: {
+          orderStatus: `approved`
+        },
+      };
+
+      const result = await bookingCollection.updateOne(filter, updateStatus, options);
+
+      res.json(result);
+    })
+    
+    // Bookings- DELETE API
+    app.delete('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+
+      res.json(result);
+    })
+
+    
 
     
 
